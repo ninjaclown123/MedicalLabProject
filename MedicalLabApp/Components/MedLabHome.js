92a0477423606344b75cf46c2d21bc79478c6207
ninjaclown123 <samsonwilliam12@gmail.com>
2023-07-10 14:40:49 +0300

    finished setting up material bottom tabs navigation

---
+++ MedicalLabApp/Components/MedLabHome.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MedLabHome = () => {
+  return (
+    <View>
+      <Text>MedLabHome</Text>
+    </View>
+  )
+}
+
+export default MedLabHome
+
+const styles = StyleSheet.create({})
